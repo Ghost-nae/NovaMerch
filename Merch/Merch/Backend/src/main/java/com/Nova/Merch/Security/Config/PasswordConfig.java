cbec3ec9ea6bee74928e9890fe27d2a7631c959d
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2026-09-25 23:44:27 +0200

    Fix formatting in PasswordConfig.java

--- Merch/Merch/Backend/src/main/java/com/Nova/Merch/Security/Config/PasswordConfig.java
+++ Merch/Merch/Backend/src/main/java/com/Nova/Merch/Security/Config/PasswordConfig.java
@@ -1,4 +1,4 @@
-package com.Nova.Merch.Security.Config
+package com.Nova.Merch.Security.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
