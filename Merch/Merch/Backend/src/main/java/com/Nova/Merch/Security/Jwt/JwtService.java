b4c0c61c66126c7577143936e248834ad5806f97
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2026-09-26 00:56:13 +0200

    Implement JwtService for token generation and management

---
+++ Merch/Merch/Backend/src/main/java/com/Nova/Merch/Security/Jwt/JwtService.java
@@ -0,0 +1,45 @@
+package com.Nova.Merch.Security.Jwt;
+
+import com.novagroup.novamerch.identity.entity.Identity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class JwtService {
+  private final JwtEncoder jwtEncoder;
+  private final Duration accessTokenExpiration;
+
+  public JwtService(JwtEncoder jwtEncoder, @Value("${jwt.access-token-expiration}") Duration accessTokenExpiration) {
+    this.jwtEncoder = jwtEncoder;
+    this.accessTokenExpiration;
+  }
+
+  public String generateAccessToken(Identity identity) {
+    Instant now = Instant.now();
+
+    JwtClaimsSet claims = JwtClaimsSet
+      .builder()
+      .issuer("novamerch")
+      .subject(identity.getId().toString())
+      .issueAt(now)
+      .expiresAt(now.plus(accessTokenExpiration))
+      .id(UUID.randomUUID().toString())
+      .claim("email", identity.getEmail())
+      .build();
+    
+    return jwtEncoder
+      .encode(JwtEncoderParameters.from(claims))
+      .getTokenValue()
+  }
+
+  public long getAccessTokenExpirationSeconds() {
+    return accessTokenExpiration.toSeconds();
+  }
+}
